8a706e5d0f4ec0251d68e82621ea203dc68badc4
aKozlovv <kozlovv.digital@gmail.com>
2024-06-21 17:21:34 +0300

    routeView | sheet cleanUp

--- Strelka/App/RoutesScreen/RoutesView.swift
+++ Strelka/App/RoutesScreen/RoutesView.swift
@@ -14,72 +14,25 @@ struct RoutesView: View {
             Marker("Mok", coordinate: .mokLocation)
                 .tint(.accent)
         }
-        .botomSheet(
-            presentationDetents: [.medium, .large, .height(70)], isPresented: .constant(true)) {
-                ScrollView(.vertical, showsIndicators: false) {
-                    HStack {
-                        Text("Your routes")
-                            .font(.title.bold().smallCaps())
-                            .foregroundStyle(.secondary)
-                        Spacer()
-                        Button {} label: {
-                            Image(systemName: "plus")
-                        }
+        .sheet(isPresented: .constant(true)) {
+            ScrollView(showsIndicators: false) {
+                HStack {
+                    Text("Your routes")
+                        .font(.title.bold().smallCaps())
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                    Button {} label: {
+                        Image(systemName: "plus")
+                            .bold()
                     }
-                    .padding(.all, 25)
                 }
-                .background(
-                    Rectangle()
-                        .fill(.regularMaterial)
-                        .ignoresSafeArea()
-                )
-            } onDismiss: {}
-            
-    }
-    
-}
-
-
-extension View {
-    @ViewBuilder
-    func botomSheet<Content: View>(
-        presentationDetents: Set<PresentationDetent>,
-        isPresented: Binding<Bool>,
-        indicatorVisibility: Visibility = .visible,
-        sheetCornerRadius: CGFloat = 20,
-        largestUndimmedIdentifier: UISheetPresentationController.Detent.Identifier = .large,
-        isTransparent: Bool = true,
-        interactiveDisabled: Bool = true,
-        @ViewBuilder content: @escaping () -> Content,
-        onDismiss: @escaping () -> Void
-    ) -> some View {
-        self
-            .sheet(isPresented: isPresented, onDismiss: { onDismiss() }) {
-                content()
-                    .presentationDetents(presentationDetents)
-                    .presentationDragIndicator(indicatorVisibility)
-                    .interactiveDismissDisabled(interactiveDisabled)
-                    .presentationBackgroundInteraction(.enabled)
-                    .onAppear {
-                        guard let windows = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-                            return
-                        }
-                        
-                        if let controller = windows.windows.first?.rootViewController?.presentedViewController,
-                           let sheet = controller.sheetPresentationController {
-                            
-                            if isTransparent {
-                                controller.view.backgroundColor = .clear
-                            }
-                            
-                            
-                            controller.presentingViewController?.view.tintAdjustmentMode = .normal
-                            
-                            sheet.largestUndimmedDetentIdentifier = largestUndimmedIdentifier
-                            sheet.preferredCornerRadius = sheetCornerRadius
-                        }
-                    }
+                .padding(.all, 25)
             }
+            .presentationDetents([.height(70), .medium, .large])
+            .presentationCornerRadius(20)
+            .presentationBackground(.regularMaterial)
+            .presentationBackgroundInteraction(.enabled(upThrough: .large))
+        }
     }
 }
 
