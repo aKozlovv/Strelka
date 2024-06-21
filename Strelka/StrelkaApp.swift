2a47a4fab94a65dd3ed6cf16efae029a5a977d54
aKozlovv <kozlovv.digital@gmail.com>
2024-06-21 14:33:41 +0300

    Initial Commit

---
+++ Strelka/StrelkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrelkaApp.swift
+//  Strelka
+//
+//  Created by Антон Козлов on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct StrelkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
