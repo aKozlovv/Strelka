35a0f636757f48daecde3450df1c3104b0080c55
aKozlovv <kozlovv.digital@gmail.com>
2025-03-21 20:00:00 +0300

    [StrelkaUI] sample app has been created

---
+++ StrelkaUI/StrelkaUISample/StrelkaUISample/StrelkaUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrelkaUISampleApp.swift
+//  StrelkaUISample
+//
+//  Created by Антон Козлов on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StrelkaUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
